66d4f9bce552bd4f0da9a985c503a034bcf1bf15
arouxel <alexandrerouxel17@gmail.com>
2023-10-13 12:07:21 +0200

    mise en place du back

---
+++ backend/src/main/java/wcs/backend/controllers/ChatController.java
@@ -0,0 +1,28 @@
+package wcs.backend.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import wcs.backend.services.ChatService;
+import wcs.backend.dtos.ChatDto;
+
+import java.util.List;
+
+@RequestMapping(path = "api/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @GetMapping
+    public List<ChatDto> getAllChats() {
+        return chatService.getAllChats();
+    }
+
+    @PostMapping
+    public ChatDto addChat(@RequestBody ChatDto chatDto) {
+        return chatService.addChat(chatDto);
+    }
+
+}
