95bc070c72d7952631f49dc67367ea5a78fa404c
Joshua <joshua35260@gmail.com>
2023-12-11 18:47:43 +0100

    fix base url partout

--- frontend/src/app/core/services/category.service.ts
+++ frontend/src/app/core/services/category.service.ts
@@ -2,18 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Category } from '../../features/ticket/models/category';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-
-  private baseUrl = "http://localhost:8080/api/categories";
+  private baseUrl = environment.apiUrl;
 
   constructor(private httpClient: HttpClient) { }
 
   getCategoryList(): Observable<Category[]>{
-    return this.httpClient.get<Category[]>(this.baseUrl);  
+    return this.httpClient.get<Category[]>(this.baseUrl+'/categories');  
   }
 
   createTicket(ticket : Category): Observable<Category[]>{
@@ -21,15 +20,15 @@ export class CategoryService {
   }
 
   getTicket(categoryId : number): Observable<Category>{
-    return this.httpClient.get<Category>("http://localhost:8080/api/categories/"+categoryId);  
+    return this.httpClient.get<Category>(this.baseUrl+"/categories/"+categoryId);  
   }
 
   updateTicket(ticket : Category, categoryId : number): Observable<Category[]>{
-    return this.httpClient.put<Category[]>("http://localhost:8080/api/categories/"+categoryId, ticket);  
+    return this.httpClient.put<Category[]>(this.baseUrl+"/categories/"+categoryId, ticket);  
   }
 
   deleteTicket(categoryId : number): Observable<Category[]>{
-    return this.httpClient.delete<Category[]>("http://localhost:8080/api/categories/"+categoryId);  
+    return this.httpClient.delete<Category[]>(this.baseUrl+"/categories/"+categoryId);  
   }
 
 }
--- frontend/src/app/core/services/ticket.service.ts
+++ frontend/src/app/core/services/ticket.service.ts
@@ -4,13 +4,13 @@ import { Observable, catchError, map, of, switchMap } from 'rxjs';
 import { Ticket } from '../../features/ticket/models/ticket';
 import { Category } from '../../features/ticket/models/category';
 import { EntityCollectionServiceBase, EntityCollectionServiceElementsFactory } from '@ngrx/data';
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketService extends EntityCollectionServiceBase<Ticket> {
-  private baseUrl = "http://localhost:8080/api/";
+  private baseUrl = environment.apiUrl;
 
   constructor(
     private httpClient: HttpClient,
@@ -20,54 +20,54 @@ export class TicketService extends EntityCollectionServiceBase<Ticket> {
    }
 
   getTicketList(): Observable<Ticket[]> {
-    return this.httpClient.get<Ticket[]>(this.baseUrl+'tickets');
+    return this.httpClient.get<Ticket[]>(this.baseUrl+'/tickets');
   }
 
   createTicket(ticket: Ticket, selectedCategory: number): Observable<Ticket[]> {
     const ticketWithCategory = { ...ticket, category: { id: selectedCategory } };
     console.log(ticketWithCategory);
-    return this.httpClient.post<Ticket[]>(this.baseUrl+'tickets', ticketWithCategory);
+    return this.httpClient.post<Ticket[]>(this.baseUrl+'/tickets', ticketWithCategory);
   }
 
   getTicket(ticketId: number): Observable<Ticket> {
-    return this.httpClient.get<Ticket>(this.baseUrl+'tickets/'+ticketId);
+    return this.httpClient.get<Ticket>(this.baseUrl+'/tickets/'+ticketId);
   }
 
   updateTicket(ticketId: number, ticket: Ticket): Observable<Ticket> {
-    return this.httpClient.put<Ticket>(this.baseUrl+'tickets/'+ticketId,ticket);
+    return this.httpClient.put<Ticket>(this.baseUrl+'/tickets/'+ticketId,ticket);
   }
 //TODO: voir si on peut refresh le ticket avec la réponse du back suite au put ?  le back renvoie l'ancien ticket, pas le nouveau suite au push en réponse, obligé de de refaire un get donc
   updateAndFetchTicket(ticketId: number, ticket: Ticket): Observable<Ticket> {
-    return this.httpClient.put<Ticket>(this.baseUrl + 'tickets/' + ticketId, ticket).pipe(
-      switchMap(() => this.httpClient.get<Ticket>(this.baseUrl + 'tickets/' + ticketId))
+    return this.httpClient.put<Ticket>(this.baseUrl + '/tickets/' + ticketId, ticket).pipe(
+      switchMap(() => this.httpClient.get<Ticket>(this.baseUrl + '/tickets/' + ticketId))
     );
   }
 
   archiveTicket(ticketId: number): Observable<HttpResponse<any>> {
-    return this.httpClient.put(this.baseUrl+'tickets/archive/'+ ticketId, null, { observe: 'response' });
+    return this.httpClient.put(this.baseUrl+'/tickets/archive/'+ ticketId, null, { observe: 'response' });
   }
   unarchiveTicket(ticketId: number): Observable<HttpResponse<any>> {
-    return this.httpClient.put(this.baseUrl+'tickets/unarchive/'+ ticketId, null, { observe: 'response' });
+    return this.httpClient.put(this.baseUrl+'/tickets/unarchive/'+ ticketId, null, { observe: 'response' });
   }
   deleteTicket(ticketId: number): Observable<Ticket[]> {
-    return this.httpClient.delete<Ticket[]>(this.baseUrl+'tickets/'+ticketId);
+    return this.httpClient.delete<Ticket[]>(this.baseUrl+'/tickets/'+ticketId);
   }
   // TicketService
   getTicketsByFilters(filter: string): Observable<Ticket[]> {
     // console.log('filters: ',filter);
-    return this.httpClient.get<Ticket[]>(this.baseUrl+'tickets/filter?'+filter);
+    return this.httpClient.get<Ticket[]>(this.baseUrl+'/tickets/filter?'+filter);
   }
   getCountTicketsByStatus(filter: string): Observable<number> {
     // console.log('filters: ',filter);
-    return this.httpClient.get<number>(this.baseUrl+'tickets/countByStatus/'+filter);
+    return this.httpClient.get<number>(this.baseUrl+'/tickets/countByStatus/'+filter);
   }
   getCountTicketsByPriority(filter: string): Observable<number> {
     // console.log('filters: ',filter);
-    return this.httpClient.get<number>(this.baseUrl+'tickets/countByPriority/'+filter);
+    return this.httpClient.get<number>(this.baseUrl+'/tickets/countByPriority/'+filter);
   }
   getCountTicketsByCategory(filter: string): Observable<number> {
     // console.log('filters: ',filter);
-    return this.httpClient.get<number>(this.baseUrl+'tickets/countByCategory/'+filter);
+    return this.httpClient.get<number>(this.baseUrl+'/tickets/countByCategory/'+filter);
   }
 
 
