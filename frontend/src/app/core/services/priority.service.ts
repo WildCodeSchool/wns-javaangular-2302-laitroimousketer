0d406d426b5681b47b55edc9c9680661e931fea8
Joshua <joshua35260@gmail.com>
2023-12-11 18:49:20 +0100

    voila

--- frontend/src/app/core/services/priority.service.ts
+++ frontend/src/app/core/services/priority.service.ts
@@ -2,18 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Priority } from 'src/app/features/ticket/models/Priority';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class PriorityService {
 
-  private baseUrl = "http://localhost:8080/api/priority";
+  private baseUrl = environment.apiUrl
 
   constructor(private httpClient: HttpClient) { }
 
   getPriorityList(): Observable<Priority[]> {
-    return this.httpClient.get<Priority[]>(this.baseUrl);
+    return this.httpClient.get<Priority[]>(this.baseUrl+'/priority');
   }
 
 }
