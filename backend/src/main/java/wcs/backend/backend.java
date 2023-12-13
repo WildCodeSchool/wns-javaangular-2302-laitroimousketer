6baf9a78b00eb4850478192d6f447f72ecc67bd7
Joshua Dupin <jdupin@topics.fr>
2023-12-13 11:09:53 +0100

    checkpoint

--- backend/src/main/java/wcs/backend/backend.java
+++ backend/src/main/java/wcs/backend/backend.java
@@ -1,10 +1,11 @@
 package wcs.backend;
 
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@PropertySource("file:../../../../../../../../.env")
 public class backend {
 
 	public static void main(String[] args) {
