a0e7a82e3898f5041621ab1ba182c07282705abe
Joshua <joshua35260@gmail.com>
2024-01-27 16:27:09 +0100

    rework jwt filter

--- backend/src/main/java/wcs/backend/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/wcs/backend/security/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import wcs.backend.services.CustomUserDetailsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,6 +19,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@DependsOn("jpaSharedEM_entityManagerFactory")
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
   @Autowired
   private JwtTokenProvider jwtTokenProvider;
